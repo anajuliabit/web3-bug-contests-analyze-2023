8b9c7f3632ddc5260746824285850faaffeb6864
Ana Julia <anajuliabit@gmail.com>
2023-10-16 15:56:40 -0300

    Adding nim scripts

---
+++ paper/.auctex-auto/final_paper.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "final_paper"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "1"))
+ '(or :bibtex :latex))
+
---
+++ paper/.auctex-auto/references.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "TechnologyTippingPoints"
+    "BlockchainAdoptionsMaritime"
+    "EthereumWhitepaper"
+    "HereHowMuch"
+    "SmartContractsMarket"))
+ '(or :bibtex :latex))
+
