294fa1a563495623330a6c907bc2d69bc4beac49
Ana Julia <anajuliabit@gmail.com>
2023-10-25 21:15:04 -0300

    Update dataset

---
+++ scripts/classfication_frequency.py
@@ -0,0 +1,15 @@
+# Importing necessary libraries for visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Count the frequency of each 'Class'
+class_frequency = df['Class'].value_counts()
+
+# Plotting the frequency of each 'Class'
+plt.figure(figsize=(12, 6))
+sns.barplot(x=class_frequency.index, y=class_frequency.values, palette="viridis")
+plt.title('Frequency of Each Class')
+plt.xlabel('Class')
+plt.ylabel('Frequency')
+plt.xticks(rotation=45)
+plt.show()
